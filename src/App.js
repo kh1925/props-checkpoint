c62add49b4d6dba87d34a851af6f6fb07be80ce8
khalil ben romdhane <kh1925@outlook.com>
2023-02-18 02:49:30 +0100

    added onClick event to profile component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Profile from "./profile/profile";
 import img from "./sheep.jpg";
 
+const handleName = (name) => alert(name);
 function App() {
   return (
     <div
@@ -15,6 +16,7 @@ function App() {
         fullName={undefined}
         bio="Simply the best web developer in Tunisia"
         profession="Web developer"
+        handleName={handleName}
       >
         <img src={img} alt="a shepard with his sheeps" style={{ width: 300 }} />
       </Profile>
--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -3,7 +3,12 @@ import PropTypes from "prop-types";
 const Profile = (props) => {
   return (
     <div>
-      <h1 style={{ color: "blue" }}>{props.fullName}</h1>
+      <h1
+        onClick={() => props.handleName(props.fullName)}
+        style={{ color: "blue" }}
+      >
+        {props.fullName}
+      </h1>
       <h2>{props.bio}</h2>
       <h3 style={{ color: "white" }}>{props.profession}</h3>
       {props.children}
@@ -17,7 +22,7 @@ Profile.defaultProps = {
 };
 
 Profile.propTypes = {
-  fullName: PropTypes.number,
+  fullName: PropTypes.string,
 };
 
 export default Profile;
